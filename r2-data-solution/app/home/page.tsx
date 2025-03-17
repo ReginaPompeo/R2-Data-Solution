3cc953c277edd11ce413018f7aec7e0bfe37d873
Regina Pompeo <111822109+ReginaPompeo@users.noreply.github.com>
2025-03-17 18:51:14 -0300

    style: apply styling to Home page

--- r2-data-solution/app/home/page.tsx
+++ r2-data-solution/app/home/page.tsx
@@ -3,8 +3,12 @@ import styles from './home.module.scss';
 export default function Home(){
     return (
         <div className={styles.homeContainer}>
-            <h1>Página Inicial</h1>
-            <p>Bem-vindo</p>
+            <div className={styles.topContainer}>
+                TOOOP
+            </div>
+            <div className={styles.centerContainer}>
+                    CENTEEEER
+            </div>
         </div>
     )
 };
